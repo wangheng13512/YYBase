1ec9b286e6e4ddcf494d55c2cf1285bef7f0c397
王恒 <1066026709@qq.com>
2022-09-16 11:06:58 +0800

    Initial Commit

---
+++ YYBase/YYBase.h
@@ -0,0 +1,18 @@
+//
+//  YYBase.h
+//  YYBase
+//
+//  Created by 王恒 on 2022/9/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YYBase.
+FOUNDATION_EXPORT double YYBaseVersionNumber;
+
+//! Project version string for YYBase.
+FOUNDATION_EXPORT const unsigned char YYBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYBase/PublicHeader.h>
+
+
